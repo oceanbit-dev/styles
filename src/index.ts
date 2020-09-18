9293a8aaf70464cd14a6956253a6c1bfc0b0c754
Corbin Crutchley <crutchcorn@gmail.com>
2020-09-17 19:01:01 -0700

    Added border radius, border, opacity, and spacing tokens

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export * from './theme';
 export * from './colors';
 export * from './color_tokens';
 export * from './text_styles';
+export * from './opacity';
+export * from './spacing';
+export * from './border_radius';
+export * from './borders';
